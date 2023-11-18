bb793df64ad0d87caf08f9f47cc53397d8613396
onwusilikegodswill@gmail.com <onwusilikegodswill@gmail.com>
2023-11-18 05:04:36 +0100

    sorts the array and prints it out

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap_elements - swap the values
+ * @element_a: the first element to swap
+ * @element_b: the second element to swap
+ *
+ * Return: void since were void
+ */
+
+void swap_elem(int *element_a, int *element_b)
+{
+	int tmp; /** temporary variable to hold the elements */
+
+	tmp = *element_a;
+	*element_a = *element_b;
+	*element_b = tmp;
+}
+
+/**
+ * bubble sort - data sorting algorithm
+ * @arry: the array to sort
+ * @size: the size of the array
+ *
+ * Return: Nothing since were void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t arr_pos, elem_pos;
+
+	/** iterate over the array first */
+	for (arr_pos = 0; arr_pos < size - 1; arr_pos++)
+	{
+		for (elem_pos = 0; elem_pos < size - arr_pos - 1; elem_pos++)
+		{
+			if (array[elem_pos] > array[elem_pos + 1])
+			{
+				swap_elem(&array[elem_pos], &array[elem_pos + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
