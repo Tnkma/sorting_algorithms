9ca0dd526b2f1cb3e932624ea95957126bb2f7d6
onwusilikegodswill@gmail.com <onwusilikegodswill@gmail.com>
2023-11-19 10:37:53 +0100

    compiled now and printing

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,7 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t elem_pos, arr_pos = 0, min_value;
+	int tmp;
 
 	/** check if the array is empty and size empty */
 	if (array == NULL || size < 2)
@@ -22,7 +23,7 @@ void selection_sort(int *array, size_t size)
 	{
 		/** keep the count of the current position as the min_value*/
 		min_value = arr_pos;
-		for (elem_pos = arr_pos + 1; elem_pos < size; elem_pos)
+		for (elem_pos = arr_pos + 1; elem_pos < size; elem_pos++)
 		{
 			/** compare the elem_pos with what in the min_value */
 			if (array[elem_pos] < array[min_value])
@@ -33,7 +34,9 @@ void selection_sort(int *array, size_t size)
 			}
 		}
 		/** swap the min_value with the very first value */
-		swap(&array[min_value], &array[arr_pos]);
+		tmp = array[min_value];
+		array[min_value] = array[arr_pos];
+		array[arr_pos] = tmp;
 		print_array(array, size);
 
 		/** increment the arr_pos and go to the next value in the array */
