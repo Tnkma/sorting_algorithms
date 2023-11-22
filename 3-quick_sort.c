d4671780b7868462dcc22451937ebcc4a8bdca8d
Tnkma <onwusilikegodswill@gmail.com>
2023-11-22 23:59:59 +0100

    fixing the quick sort issues

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,80 +1,84 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * partition - finds the partition for the quicksort using the Lomuto scheme
- * @array: array to sort
- * @low: lowest index of the partition to sort
- * @high: highest index of the partition to sort
- * @size: size of the array
+ * swap_elements - swaps the two elements
+ * @element_a: the first element
+ * @element_b: the second element
  *
- * Return: index of the partition
+ * Return: void since were void
  */
-size_t partition(int *array, ssize_t low, ssize_t high, size_t size)
-{
-	ssize_t i, j;
-	int swap, pivot;
 
-	/*selects the pivot element from the array*/
-	pivot = array[high];
-	i = low - 1;
-	for (j = low; j < high; j++)
+void swap_elements(int *element_a, int *element_b)
+{
+	int tmp;
+
+	tmp = *element_a;
+	*element_a = *element_b;
+	*element_b = tmp;
+}
+
+/**
+ * partition - the Lomuto partition scheme that partitions the array
+ * @array: the array to partition
+ * @low: the lowest number
+ * @las_num: the last element of the pivot
+ *
+ * Return: the partitioned aray
+ */
+int partition(int *array, int low, int las_num)
+{
+	int pivot = array[las_num];
+	int i = low - 1, j;
+
+	for (j = low; j < las_num; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			if (i != j)
-			{
-				swap = array[i];
-				array[i] = array[j];
-				array[j] = swap;
-				print_array(array, size);
-			}
+			swap_elements(&array[i], &array[j]);
 		}
 	}
-	if (array[high] < array[i + 1])
-	{
-		swap = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = swap;
-		print_array(array, size);
-	}
+
+	swap_elements(&array[i + 1], &array[las_num]);
+	
 	return (i + 1);
 }
 
 /**
- * quicksort - sorts a partition of an array of integers
- * @array: array to sort
- * @lo: lowest index of the partition to sort
- * @hi: highest index of the partition to sort
- * @size: size of the array
+ * recursive_call - recursively sorts the array
+ * @array: the array to sort
+ * @low: the starting index of the array
+ * @las_ele: the last element in the array
  *
- * Return: void
+ * Return: void since were void
  */
-void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
+
+void recursive_call(int *array, int low, int las_ele)
 {
-	ssize_t pivot;
+	int index;
 
-	if (low < high)
+	if (low < las_ele)
 	{
-		pivot = partition(array, low, high, size);
-		quicksort(array, low, pivot - 1, size);
-		quicksort(array, pivot + 1, high, size);
-
+		index = partition(array, low, las_ele);
+		recursive_call(array, low, index - 1);
+		recursive_call(array, index + 1, las_ele);
 	}
+	print_array(array, las_ele + 1);
 }
-
 /**
- * quick_sort - sorts an array of integers in ascending order using the
- * Quick sort algorithm
- * @array: The array to sort
- * @size: The size of the array
+ * quick_sort - sorts an array of integer using the quick_sort algorithm
+ * @array: the array of integers to sort
+ * @size: the size of the array
  *
- * Return: void
+ * Return: void since were void
  */
+
 void quick_sort(int *array, size_t size)
 {
+
 	if (array == NULL || size < 2)
+	{
 		return;
-	quicksort(array, 0, size - 1, size);
+	}
+	recursive_call(array, 0, size - 1);
 }
--- sort.h
+++ sort.h
@@ -27,9 +27,7 @@ void insertion_sort_list(listint_t **list);
 void swap(int *element_a, int *element_b);
 void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-size_t partition(int *array, ssize_t low, ssize_t high, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort(int *array, ssize_t low, ssize_t high, size_t size);
 void swaps(listint_t **list, listint_t *node);
 void cocktail_sort_list(listint_t **list);
 void Two_sort_Merge(size_t start, size_t middle, size_t end, int *dest, int *source);
@@ -39,8 +37,6 @@ void bitonic_compare(char up, int *array, size_t size);
 void bitonic_merge(char up, int *array, size_t size);
 void bit_sort(char up, int *array, size_t size, size_t t);
 void bitonic_sort(int *array, size_t size);
-size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
-void quick_sort(int *array, ssize_t lo, ssize_t hi, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
